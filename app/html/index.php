19fac972daefe82c72916d104cad3197aea90a42
James Stormes <james.s@digitalroominc.com>
2024-07-29 15:11:07 -0500

    Readme update

--- app/html/index.php
+++ app/html/index.php
@@ -1,4 +1,18 @@
 <?php
+
+
+//try {
+//    $t = 0;
+//    $i = 1 / $t;
+//    throw new Exception('This is a test exception');
+//}
+//catch (Exception $e) {
+//    echo 'Caught exception: ',  $e->getMessage(), "\n";
+//}
+
+//throw new Exception('This is a test exception');
 phpinfo();
+
+
 //echo "Hello, World!";
 error_log("Hello2, errors!");
